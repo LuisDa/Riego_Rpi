2471631664147f6c3768750f79d5cf69bd85a7b4
LuisDa <luisdavid.lopezroberts.luzon@gmail.com>
2019-06-17 16:38:36 +0000

    Añadimos un objeto de área de dibujo (drawing_area) que es un círculo, será la base para añadir LEDs de monitoreo de estado

--- main.cxx
+++ main.cxx
@@ -25,11 +25,14 @@
 #include <iostream>
 #include <bcm2835.h>
 #include <time.h>
+#include <math.h>
 
 
 unsigned char gpio_status = 0x00; //Byte para controlar el estado de cada salida de GPIO
 
-cairo_t *cr = NULL;
+//cairo_t *cr = NULL;
+GdkColor color;
+GdkRGBA rgba;
 
 /* Our new improved callback. The data passed to this function
 * is printed to stdout. */
@@ -107,11 +110,36 @@ void callback_10( GtkWidget *widget, gpointer data )
 static gboolean timer_event(GtkWidget *widget)
 {
 	g_print ("Saltose el evento del TIMER\n");
-	
+
 	return TRUE; //Si devuelve TRUE, el temporizador volverá a lanzarse, si devuelve FALSE, se detiene.
 }
 
+//Callback para dibujar
+gboolean draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+    guint width, height;
+    GdkRGBA color;
+    GtkStyleContext *context;
+    
+    g_print ("Redibujando\n");
 
+    context = gtk_widget_get_style_context (widget);
+    width = 100;//gtk_widget_get_allocated_width (widget);
+    height = 50;//gtk_widget_get_allocated_height (widget);
+    gtk_render_background(context, cr, 0, 0, width, height);
+    cairo_arc (cr, width/2.0, height/2.0, MIN (width, height) / 2.0, 0, 2 * G_PI);
+    gtk_style_context_get_color (context, gtk_style_context_get_state (context), &color);
+    
+    color.red = 1;
+    color.green = 0.4;
+    color.blue = 0;
+    color.alpha = 1;
+    
+    gdk_cairo_set_source_rgba (cr, &color);
+    gdk_cairo_set_source_rgba (cr, &color);
+    cairo_fill (cr);
+    return FALSE;
+}
 
 /* another callback */
 gint delete_event( GtkWidget *widget, GdkEvent *event, gpointer data )
@@ -137,10 +165,12 @@ int main(int argc, char **argv)
 	/* GtkWidget is the storage type for widgets */
 	GtkWidget *window;
 	GtkWidget *button;
+	GtkWidget *drawing_area;
 	GtkWidget *box1;	
 	GtkWidget *box2;	
+	GtkWidget *box3;
 	GtkWidget *hbox;
-	
+		
 	
 	/* This is called in all GTK applications. Arguments are parsed
 	* from the command line and are returned to the application. */
@@ -150,7 +180,7 @@ int main(int argc, char **argv)
 	gtk_window_set_title (GTK_WINDOW (window), "GÜINDOU");
 	gtk_window_set_default_size(GTK_WINDOW(window), 600, 500);		//Size of the the client area (excluding the additional areas provided by the window manager)
 	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-	
+		
 	g_timeout_add(1000, (GSourceFunc) timer_event, (gpointer) window); //Configuramos el temporizador para que genere un evento cada segundo (1000 ms)
 	
 	g_signal_connect (G_OBJECT (window), "delete_event", G_CALLBACK (delete_event), NULL);	
@@ -165,10 +195,13 @@ int main(int argc, char **argv)
 	box1 = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);
 	//box2 = gtk_vbox_new (FALSE, 5);
 	box2 = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);
+	
+	box3 = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);
 
 	gtk_container_add (GTK_CONTAINER (window), hbox);
 	gtk_box_pack_start(GTK_BOX(hbox), box1, FALSE, FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(hbox), box2, FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(hbox), box3, FALSE, FALSE, 0);
 	
 	button = gtk_button_new_with_label ("Button_1");		
 	gtk_widget_set_size_request(button,2,2);	
@@ -230,11 +263,21 @@ int main(int argc, char **argv)
 	gtk_box_pack_start(GTK_BOX (box2), button, FALSE, FALSE, 0);		
 	gtk_widget_show (button);
 	
+	drawing_area = gtk_drawing_area_new(); 	   
+    //gtk_container_add (GTK_CONTAINER (box3), drawing_area);
+    gtk_widget_set_size_request (drawing_area, 100, 50);    
+	g_signal_connect (G_OBJECT (drawing_area), "draw", G_CALLBACK (draw_callback), NULL);
+	gtk_box_pack_start(GTK_BOX (box3), drawing_area, FALSE, FALSE, 0);
+	gtk_widget_show (drawing_area);
 	
 	gtk_widget_show (box1);
 	gtk_widget_show (box2);
+	gtk_widget_show (box3);
 	gtk_widget_show (hbox);
 	gtk_widget_show (window);
+
+	//gdk_rgba_parse(&rgba, "#80FF00");
+	//gtk_widget_override_background_color (box3, GTK_STATE_FLAG_NORMAL, &rgba);
 	
 	gtk_main ();
 
